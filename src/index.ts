d31edf46ef063dddb44cba0478f782226e25efd5
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-09-13 21:06:28 +0600

    groupBy

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
+  const x = await prisma.user.findFirst;
   console.log("Hello Wold");
 };
 
